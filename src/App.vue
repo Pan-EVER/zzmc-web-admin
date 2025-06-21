1512b6c4a6637bda269932b12533ba600909d104
Mr.Pan <panwq.email@qq.com>
2025-06-21 21:40:25 +0800

    feat(app和专利管理): 设置组件库语言包，调整专利分类枚举

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
 import Layout from './layout/index.vue'
+import zhCN from 'ant-design-vue/es/locale/zh_CN';
 </script>
 
 <template>
-  <a-config-provider :theme="{
-    token: {
-      colorPrimary: '#efe058',
-    },
-  }">
+  <a-config-provider
+    :locale="zhCN"
+    :theme="{
+      token: {
+        colorPrimary: '#efe058',
+      },
+    }"
+  >
     <Layout>
       <RouterView />
     </Layout>
