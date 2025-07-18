cf288518319e2543a6c3454f7dba4a40d262e82b
Mr.Pan <panwq.email@qq.com>
2025-07-18 15:20:28 +0800

    refactor(产品管理): 编辑产品信息弹窗注释代码删除

--- src/views/product/component/productMaintenance.vue
+++ src/views/product/component/productMaintenance.vue
@@ -20,7 +20,6 @@
       <!-- 基本信息 -->
       <a-divider orientation="center" style="font-size: 16px">基本信息</a-divider>
       <a-form-item label="产品名称" name="name">
-        <!-- <a-input v-model:value="form.name" placeholder="请输入产品名称" /> -->
         <a-select ref="select" v-model:value="form.name" placeholder="请选择产品名称">
           <a-select-option :value="it.value" v-for="it in productNameOptions" :key="it.value">{{
             it.value
@@ -43,142 +42,17 @@
       <a-form-item name="overviewContent" :wrapper-col="{ span: 24 }">
         <TinyRichEditor v-model:content="form.overviewContent" />
       </a-form-item>
-      <!-- <a-form-item label="标题" name="title">
-        <a-input v-model:value="form.title" placeholder="请输入标题" />
-      </a-form-item>
-      <a-form-item
-        label="标题描述"
-        name="titleDescriptions"
-        :wrapper-col="wrapperCol"
-        class="titleDescriptions-item"
-      >
-        <a-input v-model:value="form.titleDescriptions[0]" placeholder="请输入文本" />
-      </a-form-item>
-      <a-form-item
-        name="titleDescriptions"
-        :wrapper-col="wrapperCol"
-        class="titleDescriptions-item"
-      >
-        <a-input v-model:value="form.titleDescriptions[1]" placeholder="请输入文本" />
-      </a-form-item>
-      <a-form-item
-        name="titleDescriptions"
-        :wrapper-col="wrapperCol"
-        class="titleDescriptions-item"
-      >
-        <a-input v-model:value="form.titleDescriptions[2]" placeholder="请输入文本" />
-      </a-form-item>
-      <a-form-item
-        :label="`内容区域${index + 1}`"
-        v-for="(item, index) in form.contentAreas"
-        :key="index"
-        :name="['contentAreas', index]"
-        :rules="index === 0 ? contentAreasItemRules(index) : []"
-        :validateTrigger="['onBlur', 'onChange']"
-      >
-        <a-row :gutter="48">
-          <a-col :span="6">
-            <a-form-item
-              no-style
-              :required="index === 0"
-              :name="['contentAreas', index, 'title']"
-              :rules="index === 0 ? contentAreasItemRules(index) : []"
-            >
-              <a-input v-model:value="item.title" :placeholder="`请输入内容区域${index + 1}标题`" />
-            </a-form-item>
-          </a-col>
-          <a-col :span="10">
-            <a-form-item
-              no-style
-              :required="index === 0"
-              :name="['contentAreas', index, 'content']"
-              :rules="index === 0 ? contentAreasItemRules(index) : []"
-            >
-              <a-textarea
-                v-model:value="item.content"
-                :auto-size="{ minRows: 2, maxRows: 5 }"
-                placeholder="请输入文本"
-              />
-            </a-form-item>
-          </a-col>
-          <a-col :span="8" class="d-flex align-items-center">
-            <a-form-item
-              :name="`contentAreas[${index}].image`"
-              :label-col="labelWidth"
-              :wrapper-col="{ span: 20 }"
-              :label="`内容区域${index + 1}图片:`"
-              no-style
-            >
-              <BaseImageUpload
-                :imageList="item.image"
-                :maxCount="1"
-                @on-change="(file) => handleImageChange(file,item)"
-              />
-            </a-form-item>
-          </a-col>
-        </a-row>
-      </a-form-item>
-      <a-form-item
-        v-for="(item, index) in form.advantages"
-        :key="index"
-        :label="`优点${index + 1}`"
-        :name="['advantages', index, 'description']"
-        :rules="index === 0 ? [{ required: true, message: `优点${index + 1}不能为空` }] : []"
-      >
-        <a-row :gutter="48">
-          <a-col :span="12">
-            <a-input v-model:value="item.description" placeholder="请输入文本" />
-          </a-col>
-          <a-col :span="12" class="d-flex align-items-center">
-            <a-form-item
-              :name="['advantages', index, 'title']"
-              :label-col="labelWidth"
-              :wrapper-col="{ span: 20 }"
-              :rules="
-                index === 0 ? [{ required: true, message: `优点${index + 1}的标题不能为空` }] : []
-              "
-              :label="`优点${index + 1}描述:`"
-              class="advantage-title"
-            >
-              <a-textarea
-                v-model:value="item.title"
-                :auto-size="{ minRows: 2, maxRows: 5 }"
-                placeholder="请输入优点描述"
-                class="custom-textarea"
-              />
-            </a-form-item>
-          </a-col>
-        </a-row>
-      </a-form-item> -->
     </a-form>
   </a-modal>
-  <!-- <a-image
-    :width="200"
-    :style="{ display: 'none' }"
-    :preview="{
-      visible: previewVisible,
-      src: previewSrc,
-      onVisibleChange: (visible) => (previewVisible = visible),
-    }"
-  /> -->
 </template>
 
 <script setup lang="ts">
-import { computed, nextTick, reactive, ref } from 'vue'
+import { nextTick, reactive, ref } from 'vue'
 import { message } from 'ant-design-vue'
-// import { PlusOutlined } from '@ant-design/icons-vue'
-// import type { UploadFile as AntUploadFile } from 'ant-design-vue'
-// import type { ContentItem } from '../types'
-
-// import { uploadFile } from '@/api/upload'
-
 import { addProducts, updateProducts } from '@/api/product/productList'
-
-// import BaseImageUpload from '../component/BaseImageUpload.vue'
 import { TinyRichEditor } from '@/components'
 
 const labelWidth = { style: { width: '100px' } }
-const wrapperCol = { span: 0 }
 
 const rules = {
   name: [{ required: true, message: '请选择产品名称' }],
@@ -186,23 +60,10 @@ const rules = {
   order: [{ required: true, message: '请输入排序权重' }],
 }
 
-// interface UploadFile extends AntUploadFile {
-//   imageId?: string
-// }
-
-// type Contents = ContentItem[]
-
 type FormType = {
   name: undefined
   description: string
   order: number
-  // title: string
-  // titleDescriptions: string[]
-  // contentAreas: Contents
-  // advantages: {
-  //   description: string
-  //   title: string
-  // }[]
   overviewContent: any
 }
 
@@ -250,103 +111,12 @@ const form = reactive<FormType>({
   description: '',
   order: 0,
   overviewContent: '',
-  // title: '',
-  // titleDescriptions: ['', '', ''],
-  // contentAreas: [
-  //   {
-  //     title: '',
-  //     content: '',
-  //     image: [],
-  //   },
-  //   {
-  //     title: '',
-  //     content: '',
-  //     image: [],
-  //   },
-  //   {
-  //     title: '',
-  //     content: '',
-  //     image: [],
-  //   },
-  // ],
-  // // 优点1
-  // advantages: [
-  //   {
-  //     description: '',
-  //     title: '',
-  //   },
-  //   {
-  //     description: '',
-  //     title: '',
-  //   },
-  //   {
-  //     description: '',
-  //     title: '',
-  //   },
-  // ],
 })
 
 const formRef = ref()
 
-// 图片预览相关
-// const previewVisible = ref(false)
-// const previewSrc = ref('')
-
-// const contentAreasItemRules = computed(() => {
-//   return (index: number) => {
-//     return [
-//       {
-//         required: true,
-//         message: '请输入内容区域内容',
-//         validator: (_, val) => validateContentArea(val, index),
-//       },
-//     ]
-//   }
-// })
-// const validateContentArea = (_, index) => {
-//   const area = form.contentAreas[index]
-//   if (!area.title && !area.content) {
-//     return Promise.reject('标题和内容至少填写一项')
-//   }
-//   return Promise.resolve()
-// }
 const initailDataFromProps = () => {
   const editInfo = JSON.parse(JSON.stringify(props.editInfo))
-  // const ensureArrayLength = (array, length, defaultValue, addImageField = false) => {
-  //   return Array.from({ length }).map((_, index) => {
-  //     const currentOne = array[index]
-  //     if (currentOne) {
-  //       if (addImageField && typeof currentOne === 'object') {
-  //         return {
-  //           ...currentOne,
-  //           image: currentOne.image ? [{ ...currentOne.image,response:{...currentOne.image} }] : [],
-  //         }
-  //       }
-  //       return currentOne
-  //     }
-  //     return defaultValue(index)
-  //   })
-  // }
-
-  // editInfo.contentAreas = ensureArrayLength(
-  //   editInfo.contentAreas || [],
-  //   3,
-  //   () => ({ title: '', content: '', image: [] }),
-  //   true,
-  // )
-  // editInfo.advantages = ensureArrayLength(
-  //   editInfo.advantages || [],
-  //   3,
-  //   () => ({ description: '', title: '' }),
-  //   false,
-  // )
-  // editInfo.titleDescriptions = ensureArrayLength(
-  //   editInfo.titleDescriptions || [],
-  //   3,
-  //   () => '',
-  //   false,
-  // )
-
   // Merge data from editInfo into form if key exists
   Object.keys(editInfo).forEach((key) => {
     if (Object.prototype.hasOwnProperty.call(form, key)) {
@@ -365,32 +135,7 @@ const showModel = () => {
   })
 }
 
-// const handleImageChange = (list: Array<UploadFile>, currentInfo) => {
-//   currentInfo.image = list
-// }
-
-// // // 控制 Loading
 const loading = ref(false)
-
-// const uploadImgs = async () => {
-//   form.contentAreas = await Promise.all(
-//     form.contentAreas.map(async (item) => {
-//       const resolvedImages = await Promise.all(
-//         item.image.map(async (img: any) => {
-//           const imageInfo = await uploadFile(img.originFileObj)
-//           return {
-//             ...img,
-//             ...imageInfo,
-//           }
-//         }),
-//       )
-//       return {
-//         ...item,
-//         image: resolvedImages,
-//       }
-//     }),
-//   )
-// }
 
 const handleOk = async () => {
   let isPass: any
@@ -401,31 +146,6 @@ const handleOk = async () => {
   }
 
   if (isPass) {
-    // await uploadImgs()
-    // const { name, title, description, titleDescriptions, contentAreas, advantages, order } = form
-    // const params = {
-    //   name,
-    //   title,
-    //   description,
-    //   order,
-    //   titleDescriptions,
-    //   contentAreas: contentAreas
-    //     .map((item) => {
-    //       return {
-    //         ...item,
-    //         image:
-    //           item.image.length > 0
-    //             ? {
-    //                 id: item.image[0]?.response?.id,
-    //                 filename: item.image[0]?.response?.originalName || item.image[0]?.response?.filename,
-    //                 url: item.image[0]?.response?.url,
-    //               }
-    //             : undefined,
-    //       }
-    //     })
-    //     .filter((i) => i.content || i.title||i.image!==undefined),
-    //   advantages: advantages.filter((it) => it.title && it.description),
-    // }
     const params = { ...form }
 
     if (props.isEdit) {
@@ -439,8 +159,6 @@ const handleOk = async () => {
       visible.value = false
     }, 1000)
   }
-  // emit('on-save', form)
-  // visible.value = false;
 }
 
 const handleCancel = () => {
@@ -451,7 +169,6 @@ const handleCancel = () => {
 const resetForm = () => {
   formRef.value.resetFields()
   form.overviewContent = ''
-  // form.contentAreas = Array.from([1, 2, 3], (_) => ({ title: '', content: '', image: [] }))
 }
 
 defineExpose({ showModel })
