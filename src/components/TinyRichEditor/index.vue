3907540b6fbbc239c3b4c754f645cd2bc82a03e9
Mr.Pan <panwq.email@qq.com>
2025-07-18 16:19:35 +0800

    fix(富文本编辑): 图片文件使用默认的base64处理

--- src/components/TinyRichEditor/index.vue
+++ src/components/TinyRichEditor/index.vue
@@ -54,12 +54,13 @@ const editorConfig = computed(() => ({
     'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough superscript subscript | align numlist bullist | link image table hr | lineheight outdent indent| forecolor backcolor removeformat |  emoticons | code | codesample',
   // toolbar: allToolbar,
   toolbar_mode: 'sliding',
+
   // 自定义图片上传处理
-  images_upload_handler: handleImageUpload,
-  // 允许的图片格式
-  images_file_types: 'jpeg,jpg,png,gif,bmp,webp',
-  // 图片上传时显示进度
-  images_upload_credentials: false,
+  // images_upload_handler: handleImageUpload,
+  // // 允许的图片格式
+  // images_file_types: 'jpeg,jpg,png,gif,bmp,webp',
+  // // 图片上传时显示进度
+  // images_upload_credentials: false,
   // 自动调整图片大小
   automatic_uploads: true,
 }))
